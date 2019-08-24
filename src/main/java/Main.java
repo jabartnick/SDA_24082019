a63d3b4d9b39b5eb09c9f84b722ead695d47287f
Jacek Bartnicki <jacekbartnicki@gmail.com>
2019-08-24 14:17:46 +0200

    rysowanie drzewka

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String[] args){
-        System.out.println("Program start");
+        System.out.println("Program start !!!!!");
 
         for(int i=0; i<10; i++){
             System.out.println(i);
