e70ae18b9ad6486d677573090cb7acf5327d57fc
Jacek Bartnicki <jacekbartnicki@gmail.com>
2019-08-24 14:09:05 +0200

    Dodano koniec programu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main (String[] args){
         System.out.println("Program start");
 
-        for(int i=0; i>10; i++){
+        for(int i=0; i<10; i++){
             System.out.println(i);
         }
+
+        System.out.println("Koniec programu");
     }
 }
